70f85a0a5b9a87fe061a1437445dccf3c43e5265
Jim <jpstrategy_clr@outlook.com>
2020-05-19 21:53:23 -0400

    Complete Mongo Model - Integrate Postman

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-management-api', {
     useNewUrlParser: true,
@@ -8,15 +9,46 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-management-api', {
 const User = mongoose.model('User',  {
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 7,
+        trim: true,
+        validate(value) {
+            if(value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 const me = new User({
-    name: 'Mike'
+    name: 'Jim',
+    email: 'omesadev@gmail.com',
+    password: 'James12345'
 })
 
 me.save().then(() => {
@@ -25,22 +57,24 @@ me.save().then(() => {
     console.log('Error: ', error)
 })
 
-// const Task = mongoose.model('Task', {
-//     description: {
-//         type: String
-//     },
-//     completed: {
-//         type: Boolean
-//     }
-// })
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
 
-// const task = new Task({
-//     description: 'Clean Pantry',
-//     completed: false
-// })
+const task = new Task({
+    description: "Dishes"
+})
 
-// task.save().then(() => {
-//     console.log(task)
-// }).catch((error) => {
-//     console.log(error)
-// })
+task.save().then(() => {
+    console.log(task)
+}).catch((error) => {
+    console.log(error)
+})
